f9d02f90e8344376447ac286098324d020abc882
Alejandro Bernal Suárez <traviscmon@gmail.com>
2025-04-11 09:12:11 +0200

    Deleted unused files and folders

--- includes/functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * @file functions.php
- * @brief This file previously contained helper functions, but they have been removed as they are now replaced by caching-enabled functions in `football_api.php`.
- */
-?>
