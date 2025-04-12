367175b86bcda7484aeab3ffe32ea9373c446238
Alejandro Bernal Suárez <traviscmon@gmail.com>
2025-04-12 20:37:40 +0200

    Added league options
    Tested the league options for all the big 5 leagues, then did the same thing for La Liga, La Liga 2, Primera RFEF, Segunda RFEF, Tercera RFEF and La Liga F

--- public/index.php
+++ public/index.php
@@ -36,8 +36,41 @@ $teamData = getDataByTeam($teamId);
                     <label for="league" class="form-label">Select League:</label>
                     <select id="league" class="form-select">
                         <option value="Premier League">Premier League</option>
-                        <option value="La Liga">La Liga</option>
                         <option value="Bundesliga">Bundesliga</option>
+                        <option value="Serie A">Serie A</option>
+                        <option value="Ligue 1">Ligue 1</option>
+                        <option value="La Liga">La Liga</option>
+                        <option value="La Liga 2">La Liga 2</option>
+                        <option value="La Liga F">La Liga F</option>
+                        <option value="Primera Division RFEF Group 1">Primera Division RFEF Group 1</option>
+                        <option value="Primera Division RFEF Group 2">Primera Division RFEF Group 2</option>
+
+                        <option value="Segunda Division RFEF Group 1">Segunda Division RFEF Group 1</option>
+                        <option value="Segunda Division RFEF Group 2">Segunda Division RFEF Group 2</option>
+                        <option value="Segunda Division RFEF Group 3">Segunda Division RFEF Group 3</option>
+                        <option value="Segunda Division RFEF Group 4">Segunda Division RFEF Group 4</option>
+                        <option value="Segunda Division RFEF Group 5">Segunda Division RFEF Group 5</option>
+
+                        <option value="Tercera Division RFEF Group 1">Tercera Division RFEF Group 1</option>
+                        <option value="Tercera Division RFEF Group 2">Tercera Division RFEF Group 2</option>
+                        <option value="Tercera Division RFEF Group 3">Tercera Division RFEF Group 3</option>
+                        <option value="Tercera Division RFEF Group 4">Tercera Division RFEF Group 4</option>
+                        <option value="Tercera Division RFEF Group 5">Tercera Division RFEF Group 5</option>
+                        <option value="Tercera Division RFEF Group 6">Tercera Division RFEF Group 6</option>
+                        <option value="Tercera Division RFEF Group 7">Tercera Division RFEF Group 7</option>
+                        <option value="Tercera Division RFEF Group 8">Tercera Division RFEF Group 8</option>
+                        <option value="Tercera Division RFEF Group 9">Tercera Division RFEF Group 9</option>
+                        <option value="Tercera Division RFEF Group 10">Tercera Division RFEF Group 10</option>
+                        <option value="Tercera Division RFEF Group 11">Tercera Division RFEF Group 11</option>
+                        <option value="Tercera Division RFEF Group 12">Tercera Division RFEF Group 12</option>
+                        <option value="Tercera Division RFEF Group 13">Tercera Division RFEF Group 13</option>
+                        <option value="Tercera Division RFEF Group 14">Tercera Division RFEF Group 14</option>
+                        <option value="Tercera Division RFEF Group 15">Tercera Division RFEF Group 15</option>
+                        <option value="Tercera Division RFEF Group 16">Tercera Division RFEF Group 16</option>
+                        <option value="Tercera Division RFEF Group 17">Tercera Division RFEF Group 17</option>
+                        <option value="Tercera Division RFEF Group 18">Tercera Division RFEF Group 18</option>
+                        
+
                         <!-- Add more leagues as needed -->
                     </select>
                 </div>
